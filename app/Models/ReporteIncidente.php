c7c66097b600cfc1a1ee81ac7b18aa716e883cff
Edgar15vulkan <edgar.kantun@uimqroo.edu.mx>
2025-09-14 20:41:03 -0500

    se creo la migracion, modelo y controlador de ReporteIncidente, alta hacer su controlador y rutas para empezar la vista

---
+++ app/Models/ReporteIncidente.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ReporteIncidente extends Model
+{
+    //
+    use HasFactory;
+
+    protected $table = 'reporte_incidentes'; // nombre de la tabla
+
+    protected $primaryKey = 'id'; // PK por defecto, no es necesario pero lo dejamos claro
+
+    protected $fillable = [
+        'titulo',
+        'personal_id',
+        'fecha',
+        'grupo',
+        'descripcion',
+        'archivo',
+    ];
+
+    /** Relación con el autor (datos_personales).*/
+    public function autor()
+    {
+        return $this->belongsTo(Personal::class, 'personal_id', 'personal_id');
+    }
+}
